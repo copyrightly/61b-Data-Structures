3fe794466b35f66a750f7347b4630e26d47f53a3
luwei <33728932+copyrightly@users.noreply.github.com>
2018-08-31 23:11:20 -0400

    hw 8

---
+++ hw8/ListSorts.java
@@ -0,0 +1,194 @@
+/* ListSorts.java */
+
+import list.*;
+
+public class ListSorts {
+
+    private final static int SORTSIZE = 10000;
+
+    /**
+     *  makeQueueOfQueues() makes a queue of queues, each containing one item
+     *  of q.  Upon completion of this method, q is empty.
+     *  @param q is a LinkedQueue of objects.
+     *  @return a LinkedQueue containing LinkedQueue objects, each of which
+     *    contains one object from q.
+     **/
+    public static LinkedQueue makeQueueOfQueues(LinkedQueue q) {
+        // Replace the following line with your solution.
+        try {
+            if (q.size() == 0) {
+                return null;
+            } else {
+                LinkedQueue qOfqs = new LinkedQueue();
+                while (q.size() > 0) {
+                    LinkedQueue curQ = new LinkedQueue();
+                    curQ.enqueue(q.dequeue());
+                    qOfqs.enqueue(curQ);
+                }
+                return qOfqs;
+            }
+        }
+        catch (QueueEmptyException e) {
+            System.out.println("ERROR1");
+            return null;
+        }
+    }
+
+    /**
+     *  mergeSortedQueues() merges two sorted queues into a third.  On completion
+     *  of this method, q1 and q2 are empty, and their items have been merged
+     *  into the returned queue.
+     *  @param q1 is LinkedQueue of Comparable objects, sorted from smallest
+     *    to largest.
+     *  @param q2 is LinkedQueue of Comparable objects, sorted from smallest
+     *    to largest.
+     *  @return a LinkedQueue containing all the Comparable objects from q1
+     *   and q2 (and nothing else), sorted from smallest to largest.
+     **/
+    public static LinkedQueue mergeSortedQueues(LinkedQueue q1, LinkedQueue q2) {
+        // Replace the following line with your solution.
+        try {
+            LinkedQueue merged = new LinkedQueue();
+            while (q1.size() != 0 && q2.size() != 0) {
+                if (((Comparable) q1.front()).compareTo(((Comparable) q2.front())) <= 0) {
+                    merged.enqueue(q1.dequeue());
+                } else {
+                    merged.enqueue(q2.dequeue());
+                }
+            }
+            if (q1.size() == 0) {
+                merged.append(q2);
+            } else {
+                merged.append(q1);
+            }
+            return merged;
+        }
+        catch (QueueEmptyException e) {
+            System.out.println("ERROR2");
+            return null;
+        }
+    }
+
+    /**
+     *  partition() partitions qIn using the pivot item.  On completion of
+     *  this method, qIn is empty, and its items have been moved to qSmall,
+     *  qEquals, and qLarge, according to their relationship to the pivot.
+     *  @param qIn is a LinkedQueue of Comparable objects.
+     *  @param pivot is a Comparable item used for partitioning.
+     *  @param qSmall is a LinkedQueue, in which all items less than pivot
+     *    will be enqueued.
+     *  @param qEquals is a LinkedQueue, in which all items equal to the pivot
+     *    will be enqueued.
+     *  @param qLarge is a LinkedQueue, in which all items greater than pivot
+     *    will be enqueued.
+     **/
+    public static void partition(LinkedQueue qIn, Comparable pivot,
+                                 LinkedQueue qSmall, LinkedQueue qEquals,
+                                 LinkedQueue qLarge) {
+        // Your solution here.
+        try {
+            while (qIn.size() > 0) {
+                if (pivot.compareTo((Comparable) qIn.front()) > 0) {
+                    qSmall.enqueue(qIn.dequeue());
+                } else if (pivot.compareTo((Comparable) qIn.front()) == 0) {
+                    qEquals.enqueue(qIn.dequeue());
+                } else {
+                    qLarge.enqueue(qIn.dequeue());
+                }
+            }
+        }
+        catch (QueueEmptyException e) {
+            System.out.println("ERROR PARTITION");
+        }
+    }
+
+    /**
+     *  mergeSort() sorts q from smallest to largest using mergesort.
+     *  @param q is a LinkedQueue of Comparable objects.
+     **/
+    public static void mergeSort(LinkedQueue q) {
+        // Your solution here.
+        try {
+            LinkedQueue qOfqs = makeQueueOfQueues(q);
+            while (qOfqs.size() > 1) {
+                qOfqs.enqueue(mergeSortedQueues((LinkedQueue) qOfqs.dequeue(), (LinkedQueue) qOfqs.dequeue()));
+            }
+            if (qOfqs.size() == 1) {
+                q.append((LinkedQueue) qOfqs.dequeue());
+            }
+        }
+        catch (QueueEmptyException e) {
+            System.out.println("ERROR3");
+        }
+    }
+
+    /**
+     *  quickSort() sorts q from smallest to largest using quicksort.
+     *  @param q is a LinkedQueue of Comparable objects.
+     **/
+    public static void quickSort(LinkedQueue q) {
+        // Your solution here.
+        if (q.size() > 1) {
+            Comparable pivot = (Comparable) q.nth((int) (Math.random() * q.size()) + 1);
+            LinkedQueue qSmall = new LinkedQueue();
+            LinkedQueue qEqual = new LinkedQueue();
+            LinkedQueue qLarge = new LinkedQueue();
+            partition(q, pivot, qSmall, qEqual, qLarge);
+            quickSort(qSmall);
+            quickSort(qLarge);
+            q.append(qSmall);
+            q.append(qEqual);
+            q.append(qLarge);
+        }
+    }
+
+    /**
+     *  makeRandom() builds a LinkedQueue of the indicated size containing
+     *  Integer items.  The items are randomly chosen between 0 and size - 1.
+     *  @param size is the size of the resulting LinkedQueue.
+     **/
+    public static LinkedQueue makeRandom(int size) {
+        LinkedQueue q = new LinkedQueue();
+        for (int i = 0; i < size; i++) {
+            q.enqueue(new Integer((int) (size * Math.random())));
+        }
+        return q;
+    }
+
+    /**
+     *  main() performs some tests on mergesort and quicksort.  Feel free to add
+     *  more tests of your own to make sure your algorithms works on boundary
+     *  cases.  Your test code will not be graded.
+     **/
+    public static void main(String [] args) {
+
+        LinkedQueue q = makeRandom(10);
+        System.out.println(q.toString());
+        mergeSort(q);
+        System.out.println(q.toString());
+
+        q = makeRandom(10);
+        System.out.println(q.toString());
+        quickSort(q);
+        System.out.println(q.toString());
+
+    // /* Remove these comments for Part III.
+    Timer stopWatch = new Timer();
+    q = makeRandom(SORTSIZE);
+    stopWatch.start();
+    mergeSort(q);
+    stopWatch.stop();
+    System.out.println("Mergesort time, " + SORTSIZE + " Integers:  " +
+                       stopWatch.elapsed() + " msec.");
+
+    stopWatch.reset();
+    q = makeRandom(SORTSIZE);
+    stopWatch.start();
+    quickSort(q);
+    stopWatch.stop();
+    System.out.println("Quicksort time, " + SORTSIZE + " Integers:  " +
+                       stopWatch.elapsed() + " msec.");
+    // */
+    }
+
+}
